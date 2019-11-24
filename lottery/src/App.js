94db01e23c5dc90a3241c28a6401f625c0ab7035
Vonds <vonds13@gmail.com>
2019-11-24 15:24:57 -0500

    lottery

---
+++ lottery/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Lottery from './Lottery';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Lottery />
+        <Lottery title="Mini Daily" numBalls={4} />
+      </div>
+    );
+  }
+}
+
+export default App;
