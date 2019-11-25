70322c3cd5ab6a94859425fabbf1fb86507ac1e7
Vonds <vonds13@gmail.com>
2019-11-25 06:49:23 -0500

    simple-form

---
+++ simple-form/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form';
+import './App.css';
+
+function App() {
+  return (
+    <section className="App">
+      <h1>Simple Form</h1>
+      <Form />
+    </section>
+  );
+}
+
+export default App;
---
+++ simple-form/src/Form.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+
+class Form extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { username: '' }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({ username: e.target.value })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        alert(`You typed: ${this.state.username}`)
+        this.setState({ username: '' })
+    }
+
+    render() {
+        return (
+            <section className="Form">
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='username'>User Name</label>
+                    <input
+                        name='username'
+                        type='text'
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <button>Submit</button>
+                </form>
+            </section>
+        )
+    }
+}
+
+export default Form
