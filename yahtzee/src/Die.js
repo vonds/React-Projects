e3b4d89d5211cd30f439553a6a6b6a590a8f2c70
Vonds <vonds13@gmail.com>
2019-11-27 14:18:08 -0500

    replaced text with icons

--- yahtzee/src/Die.js
+++ yahtzee/src/Die.js
@@ -2,25 +2,23 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numberWords: ["one", "two", "three", "four", "five", "six"]
+  }
   constructor(props) {
     super(props)
     this.handleClick = this.handleClick.bind(this)
   }
-  
+
   handleClick() {
     this.props.handleClick(this.props.idx)
   }
 
   render() {
-    return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
-    );
+    const { val, numberWords, locked, disabled} = this.props
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `
+    if (locked) classes += 'Die-locked'
+    return <i className={classes} onClick={this.handleClick} disabled={disabled}/>
   }
 }
 
