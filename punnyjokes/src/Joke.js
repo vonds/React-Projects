46de5c737bfb298301dc7da5e02c58238095657a
Vonds <vonds13@gmail.com>
2019-11-28 12:13:27 -0500

    styled the Joke component

--- punnyjokes/src/Joke.js
+++ punnyjokes/src/Joke.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
+import './Joke.css'
 
 class Joke extends Component {
-
 
     render() {
         return (
             <section className='Joke'>
                 <div className='Joke-buttons'>
                     <i className='fas fa-arrow-up' onClick={this.props.upvote} />
-                    <span>{this.props.votes}</span>
+                    <span className='Joke-votes'>{this.props.votes}</span>
                     <i className='fas fa-arrow-down' onClick={this.props.downvote} />
                 </div>
                 <div className='Joke-text'>{this.props.text}</div>
+                <div className='Joke-smiley'>
+                    <i className="em em-rolling_on_the_floor_laughing" />
+                </div>
             </section>
         )
     }
