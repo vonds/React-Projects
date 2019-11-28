1322d2cd08f983c0360e2a0ace147856e7ee6861
Vonds <vonds13@gmail.com>
2019-11-28 12:45:14 -0500

    added dynamic colors and emoji

--- punnyjokes/src/Joke.js
+++ punnyjokes/src/Joke.js
@@ -2,18 +2,39 @@ import React, { Component } from 'react'
 import './Joke.css'
 
 class Joke extends Component {
+    getColor() {
+        if (this.props.votes >= 15) return "#4CAF50"
+        if (this.props.votes >= 12) return "#8BC34A"
+        if (this.props.votes >= 9) return "#CDDC39"
+        if (this.props.votes >= 6) return "#FFEB3B"
+        if (this.props.votes >= 3) return "#FFC107"
+        if (this.props.votes >= 0) return "#FF9800"
+        if (this.props.votes < 0 ) return "#f44336"
+    }
 
+    getEmoji() {
+        if (this.props.votes >= 15) return "em em-rolling_on_the_floor_laughing"
+        if (this.props.votes >= 12) return "em em-laughing"
+        if (this.props.votes >= 9) return "em em-smiley"
+        if (this.props.votes >= 6) return "em em-slightly smiley"
+        if (this.props.votes >= 3) return "em em-neutral-face"
+        if (this.props.votes >= 0) return "em em-confused"
+        if (this.props.votes < 0) return "em em-angry"
+    }
+    
     render() {
         return (
             <section className='Joke'>
                 <div className='Joke-buttons'>
                     <i className='fas fa-arrow-up' onClick={this.props.upvote} />
-                    <span className='Joke-votes'>{this.props.votes}</span>
+                    <span className='Joke-votes' style={{borderColor: this.getColor()}}>
+                        {this.props.votes}
+                    </span>
                     <i className='fas fa-arrow-down' onClick={this.props.downvote} />
                 </div>
                 <div className='Joke-text'>{this.props.text}</div>
                 <div className='Joke-smiley'>
-                    <i className="em em-rolling_on_the_floor_laughing" />
+                    <i className={this.getEmoji()}/>
                 </div>
             </section>
         )
