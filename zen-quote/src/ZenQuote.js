66f7a0be13b814a83967debd392d9706a6d28bd8
Vonds <vonds13@gmail.com>
2019-11-27 21:47:58 -0500

    added loading animation

--- zen-quote/src/ZenQuote.js
+++ zen-quote/src/ZenQuote.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import './ZenQuote.css'
 
 class ZenQuote extends Component {
     constructor(props) {
         super(props)
-        this.state = { quote: '' }
+        this.state = { quote: '' , isLoaded: false}
     }
     componentDidMount() {
         axios.get("https://api.github.com/zen")
-            .then(response => this.setState({ quote: response.data}))
+                .then(response => {
+                    setTimeout(() => this.setState({ quote: response.data, isLoaded: true }))   
+                }, 3000) 
     }
-    
+
     render() {
         return (
             <section className='ZenQuote'>
-                <h1>Always remember...</h1>
-                <p>{this.state.quote}</p>
+            {this.state.isLoaded ? (
+                <section>
+                    <h1>Always remember...</h1>
+                    <p>{this.state.quote}</p>
+                </section>
+            ) : (
+                <section className='loader'></section>
+            )}
             </section>
         )
     }
