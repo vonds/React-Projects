e02cde54dd8ee476fa2784ceddf7dac1a9f45af3
Vonds <vonds13@gmail.com>
2019-11-28 19:26:14 -0500

    styled fetch button

--- punnyjokes/src/JokeList.js
+++ punnyjokes/src/JokeList.js
@@ -77,7 +77,7 @@ class JokeList extends Component {
                 <section className='JokeList-sidebar'>
                     <h1 className='JokeList-title'><span>Punny</span> Jokes</h1>
                     <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'></img>
-                    <button className='Jokelist-getmore' onClick={this.handleClick}>New Jokes</button>
+                    <button className='JokeList-getmore' onClick={this.handleClick}>New Jokes</button>
                 </section>
                 <section className='JokeList-jokes'>
                     {jokes.map(joke => (
