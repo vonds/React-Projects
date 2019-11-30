b5fe22f12cd70f1015eafbc019c73c39b3261541
Vonds <vonds13@gmail.com>
2019-11-30 16:18:58 -0500

    added unstyled slider

--- react-color/src/App.js
+++ react-color/src/App.js
@@ -5,12 +5,11 @@ import { generatePalette } from './colorHelpers'
 
 class App extends Component {
   
-  render() {
-    console.log(generatePalette(seedColors[2]))
+  render() { 
     return (
       <section className="App">
         <h1>React Colors App</h1>
-        <Palette {...seedColors[2]}/>
+        <Palette palette={generatePalette(seedColors[4])} />
       </section>
     )
   }
