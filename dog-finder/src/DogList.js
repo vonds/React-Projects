d1156330687f8d1744f12155ef680d8d21f81702
Vonds <vonds13@gmail.com>
2019-11-30 09:43:02 -0500

    added styling

--- dog-finder/src/DogList.js
+++ dog-finder/src/DogList.js
@@ -1,22 +1,23 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 import './DogList.css'
 
 class DogList extends Component {
     render() {
         return (
             <section className='DogList'>
-                <h1 className='display-1 text-center'>Dog List</h1>
-                <section className='container'>
+                <h1 className='display-1 text-center mb-5'>Dog List</h1>
                     <section className='row'>
                         {this.props.dogs.map(dog => (
                             <section className='Dog col-lg-4 text-center' key={dog.name}>
-                                <img src={dog.src} alt={dog.name} />
-                                <h3>{dog.name}</h3>
+                                <Link to={`dogs/${dog.name}`} >
+                                    <img src={dog.src} alt={dog.name} />
+                                    <h3 className='mt-3 underline'>{dog.name}</h3>
+                                </Link> 
                             </section>
                         ))}    
                     </section>
                 </section>
-            </section>
         )
     }
 }
