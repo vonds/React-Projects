c606a252f8f9130967018d8b192804c879358f72
Vonds <vonds13@gmail.com>
2019-11-30 15:58:16 -0500

    added colorHelpers

--- react-color/src/App.js
+++ react-color/src/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react'
 import Palette from './Palette'
 import seedColors from './seedColors'
+import { generatePalette } from './colorHelpers'
 
 class App extends Component {
+  
   render() {
+    console.log(generatePalette(seedColors[2]))
     return (
       <section className="App">
         <h1>React Colors App</h1>
