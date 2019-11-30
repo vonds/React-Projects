2180ccaf1c9c0d92bcd9fe7c6020e55667e1c332
Vonds <vonds13@gmail.com>
2019-11-30 11:41:22 -0500

    created Palette component

--- react-color/src/App.js
+++ react-color/src/App.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import Palette from './Palette'
+import seedColors from './seedColors'
 
 class App extends Component {
   render() {
     return (
       <section className="App">
         <h1>React Colors App</h1>
+        <Palette {...seedColors[4]}/>
       </section>
     )
   }
---
+++ react-color/src/Palette.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <section className='Palette'>
+                {/* Navbar goes here */}
+                <section className='Palette-colors'>
+                    {/*  Color boxes */}
+                </section>
+                {/* footer */}
+            </section>
+        )
+    }
+}
+
+export default Palette
