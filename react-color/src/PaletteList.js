aae7a21c3511158785f1d5e86dd444208f72025a
Vonds <vonds13@gmail.com>
2019-11-30 23:16:53 -0500

    initial styling of PaletteList component

--- react-color/src/PaletteList.js
+++ react-color/src/PaletteList.js
@@ -1,21 +1,56 @@
 import React, {Component} from 'react'
 import MiniPalette from './MiniPalette'
 import { Link } from 'react-router-dom'
+import { withStyles } from '@material-ui/styles'
+
+const styles = {
+    root: {
+        backgroundColor: 'blue',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    }, 
+    container: {
+        width: '50%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+        flexWrap: 'wrap'
+    },
+    nav: {
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
+        color: 'white'
+    },
+    palettes: {
+        boxSizing: 'border-box',
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 30%)',
+        gridGap: '5%'
+    }
+}
 
 class PaletteList extends Component {
     render() {
-        const { palettes } = this.props
+        const { palettes, classes } = this.props
         return (
-            <section className='PaletteList'>
-            <MiniPalette />
-                <h1>React Colors</h1>
-                { palettes.map(palette => (
-                   <MiniPalette {...palette}/>
-                    
-                )) }
+            <section className={classes.root}>
+                <section className={classes.container}>
+                    <nav className={classes.nav}>
+                        <h1>React Colors</h1>
+                    </nav>
+                    <section className={classes.palettes}>
+                        {palettes.map(palette => (
+                            <MiniPalette {...palette} />
+                        ))}
+                    </section>
+                </section>
             </section>
         )
     }
 }
 
-export default PaletteList
+export default withStyles(styles)(PaletteList)
